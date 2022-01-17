d92bbe7122aaf29c5d53ae88a9f322f7537d0128
Behnam <behnam.bozorgi71@gmail.com>
2022-01-17 03:32:02 +0100

    angular done

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
+import flask
 from flask import Flask, render_template, request, redirect, url_for
+from flask_cors import CORS, cross_origin
 import database as db
 from json import dumps
 
+api_v2_cors_config = {
+  "origins": ["http://localhost:4200"], # Angular Container IP
+  "methods": ["DELETE", "GET", "POST"],
+  "allow_headers": ["Authorization", "Content-Type"]
+}
+
 db.init()
 app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    list = db.list()
-    return render_template("index.html", todo=list, id=1, title=2, description=3)
+CORS(app, resources={
+    r"/new": api_v2_cors_config,
+    r"/edit/*": api_v2_cors_config,
+    r"/delete/*": api_v2_cors_config
+})
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route("/new", methods=["POST"])
+@cross_origin(**api_v2_cors_config)
 def new():
-    task = request.form["task"]
-    description = request.form["description"]
+    task = request.json["title"]
+    description = request.json["description"]
     id = db.register(task, description)
     return (str(id), 201)
 
@@ -23,38 +32,33 @@ def new():
 
 
 @app.route("/edit/<int:id>", methods=["POST"])
+@cross_origin(**api_v2_cors_config)
 def edit(id):
     if not db.check(id):
         return "Record not found", 400
-    task = request.form["task"]
-    description = request.form["description"]
+    task = request.json["title"]
+    description = request.json["description"]
     db.updateTitle(id, task)
     db.updateDescription(id, description)
     return ("", 204)
 
 
 @app.route("/tasks", methods=["GET"])
+@cross_origin(**api_v2_cors_config)
 def list():
     list = db.list()
+    response = flask.jsonify(list)
+    response.headers.add('Access-Control-Allow-Origin', '*')
     return dumps(list)
 
 
-@app.route("/<int:id>", methods=["DELETE"])
+@app.route("/delete/<int:id>", methods=["DELETE"])
+@cross_origin(**api_v2_cors_config)
 def delete(id):
     if not db.check(id):
         return "Record not found", 400
     db.delete(id)
     return ("", 204)
-
-
-@app.route("/test")
-def test():
-    return render_template("test.html")
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
